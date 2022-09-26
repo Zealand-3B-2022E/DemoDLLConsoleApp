a5ffd12446595c20d785276556aa6a1e4706436c
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2022-09-26 13:39:10 +0200

    Add project files.

---
+++ DemoDLLConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using StudentClassLibrary;
+
+Console.WriteLine("Hello, World!");
+
+Student student = new Student();
+student.MyName = "Martin";
+
+Console.WriteLine(student.MyNameText());
